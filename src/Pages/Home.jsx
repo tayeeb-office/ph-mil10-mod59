674b1301fe6cca7191eb98c65711b8c4e7c554d3
tayeeb-office <tayeeb.office@gmail.com>
2025-12-04 17:42:28 +0600

    commit 5

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { use } from "react";
+import { Link, Links } from "react-router";
 
 import hero1 from "../assets/Hero01.jpg";
 import hero2 from "../assets/Hero02.jpg";
@@ -23,7 +24,14 @@ import banner4 from "../assets/banner04.jpg";
 import banner5 from "../assets/banner05.jpg";
 import banner6 from "../assets/banner06.jpg";
 
+  const dataPromise = fetch("http://localhost:3000/services").then((res) =>
+    res.json()
+  );
+
 const Home = () => {
+
+    const datas = use(dataPromise);
+
   return (
     <div>
       <title>Home</title>
@@ -153,6 +161,61 @@ const Home = () => {
           </div>
         </div>
       </section>
+
+      {/* Recent Item */}
+       <div className="ml-[20px] md:w-6xl md:mx-auto py-16">
+        <h2 className="text-3xl  font-bold pb-8">Newest Arrivals</h2>
+
+        <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+          {datas?.slice(-6).reverse().map((item) => (
+            <div
+              key={item._id}
+              className="w-full rounded-3xl border border-green-200/70 bg-white"
+            >
+              <div className="p-5">
+                <div className="overflow-hidden rounded-2xl">
+                  <img
+                    src={item.image}
+                    alt={item.name}
+                    className="h-72 w-full object-cover"
+                  />
+                </div>
+
+                <div className="mt-5">
+                  <h3 className="text-xl font-extrabold text-slate-900">
+                    {item.name}
+                  </h3>
+
+                  <div className="mt-2 flex items-center justify-between text-sm font-medium text-green-700/80">
+                    <span >{item.category}</span>
+                    <span className=" pl-3 text-right">
+                      {item.location}
+                    </span>
+                  </div>
+
+                  <div className="mt-3 text-2xl font-extrabold text-slate-900">
+                    ${Number(item.price)}
+                  </div>
+
+                  <Link to={`/all/${item?._id}`}>
+                  <button
+                    type="button"
+                    className="mt-5 h-12 w-full rounded-2xl bg-lime-400 font-semibold text-slate-900 hover:bg-lime-300"
+                  >
+                    See Details
+                  </button>
+                  </Link>
+
+                  <div className="mt-4 flex flex-wrap items-center justify-between gap-2 text-xs text-slate-500">
+                    <span className="font-medium">{item.email}</span>
+                    <span>{item.date}</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
 
       {/* Why Adopt from PawMart? */}
 
